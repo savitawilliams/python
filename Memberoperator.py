bfc6f4b447d174dd940967135e3b502655d3ff10
savitawilliams <109063842+savitawilliams@users.noreply.github.com>
2022-07-25 12:13:57 +0530

    Create Memberoperator.py

---
+++ Memberoperator.py
@@ -0,0 +1,14 @@
+#python prgram to illustrate
+#not 'in' operator
+x=24
+y=20
+list=[10,20,30,40,50]
+
+if (x in list):
+    print("X is not in list")
+else:
+    print("X is in list")
+if(y in list):
+    print("Y is not in list")
+else:
+    print("Y is in list")
