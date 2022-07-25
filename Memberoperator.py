10d0ddfda6f86bac6f19426bdd88bc60aafc24f6
savitawilliams <109063842+savitawilliams@users.noreply.github.com>
2022-07-25 12:18:27 +0530

    Update Memberoperator.py

--- Memberoperator.py
+++ Memberoperator.py
@@ -4,11 +4,16 @@ x=24
 y=20
 list=[10,20,30,40,50]
 
-if (x in list):
-    print("X is not in list")
-else:
+# if (x in list):
+#     print("X is not in list")
+# else:
+#     print("X is in list")
+# if(y in list):
+#     print("Y is not in list")
+# else:
+#     print("Y is in list")
+
+if (x not in list):
     print("X is in list")
-if(y in list):
-    print("Y is not in list")
 else:
-    print("Y is in list")
+    print("X is not in list")
