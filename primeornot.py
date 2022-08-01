1ea48ec449c214bbf6b1d0c6d0e2cada6d56ef28
savitawilliams <109063842+savitawilliams@users.noreply.github.com>
2022-08-01 14:04:03 +0530

    assignment

---
+++ primeornot.py
@@ -0,0 +1,22 @@
+# Program to check if a number is prime or not
+
+num = 29
+
+#num = int(input("Enter a number: "))
+
+flag = False
+
+# prime numbers are greater than 1
+if num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+if flag:
+    print(num, "is not a prime number")
+else:
+    print(num, "is a prime number")
