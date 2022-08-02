85fdec025b6ba0b2d838185643372d67e1112978
savitawilliams <109063842+savitawilliams@users.noreply.github.com>
2022-08-02 13:35:57 +0530

    test

--- PythonProgram45.py
+++ PythonProgram45.py
@@ -1,7 +1,12 @@
 # Python program to Delete all Elements in List
 
 a = [1, 2, 3, 4, 5]
- 
-print(a)    
-a.clear()
-print(a)    
+b = ['a', 'b', 'c', 'd', 'e']   
+#print(cmp(a, b))
+# print(max(a))
+# print(min(a))    
+# print(max(a))
+# #a.clear()
+# print(a)    
+a.append(b)
+print(a)
